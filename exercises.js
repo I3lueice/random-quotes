443060794bbb7ba7dd4948ad838a75bd01403f68
I3lueice <97864265+I3lueice@users.noreply.github.com>
2022-02-07 22:15:30 -0500

    February 7, 2022 10:15 PM

--- exercises.js
+++ exercises.js
@@ -36,3 +36,13 @@ function flattening(inputList) {
     return inputList.reduce((aE1, aE2) => aE1.concat(aE2))
 }
 console.log(flattening(arrays))
+
+/* Write a higher-order function loop that provides something like a for loop statement. It takes a value, a test function, an update function, 
+and a body function. Each iteration, it first runs the test function on the current loop value and stops if that returns false. 
+Then it calls the body function, giving it the current value. Finally, it calls the update function to create a new value and starts from the beginning.
+
+ (Links to an external site.)When defining the function, you can use a regular loop to do the actual looping. */
+
+function loop(value,test, update) {
+
+}
