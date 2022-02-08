7cce5160200938ba7aa9b51abd7dd7db339e5c3f
I3lueice <97864265+I3lueice@users.noreply.github.com>
2022-02-07 21:24:39 -0500

    February 7, 2022 9:24 PM

--- exercises.js
+++ exercises.js
@@ -8,7 +8,7 @@ let myFunc = function () {
 
 let myFuncArrow = () => 1
 
-let myFuncArrow2 = () => {return 1}
+let myFuncArrow2 = () => { let a = 1; return 1 }
 
 function myHigherOrderFunction() {
 
