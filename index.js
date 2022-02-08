baf4b186b188a10d7b1e6ed6300968c4f43d7bc9
I3lueice <97864265+I3lueice@users.noreply.github.com>
2022-02-07 20:50:45 -0500

    February 7, 2022 8:50 PM

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-let quotes = [`"This is my favorite quote."`,
-`"This is my second favorite quote."`,
-`"This is my third favorite quote."`]
+let quotes = [`"In this business you either sink or swim or you don't. - David Smith"`,
+`"Friends may come and go, but enemies accumulate. - Thomas Jones "`,
+`"In three words I can sum up everything I've learned about life: it goes on. - Robert Frost"`]
 // console.log(Math.random())
 // console.log((3 * Math.random()))
 // console.log(Math.floor((3 * Math.random())))
