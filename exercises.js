372e6a5b5a7f113d5ca6f06206bdba96f51338a7
I3lueice <97864265+I3lueice@users.noreply.github.com>
2022-02-07 21:27:23 -0500

    February 7, 2022 9:27 PM

--- exercises.js
+++ exercises.js
@@ -6,7 +6,8 @@ let myFunc = function () {
     return 1 ;
 }
 
-let myFuncArrow = () => 1
+let myFuncArrow = a => a + 1
+console.log(myFuncArrow(50))
 
 let myFuncArrow2 = () => { let a = 1; return 1 }
 
