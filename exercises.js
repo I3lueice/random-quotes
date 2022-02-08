6f5d7e4777894d946c32665d2c97b0af17968021
I3lueice <97864265+I3lueice@users.noreply.github.com>
2022-02-07 22:01:59 -0500

    February 7, 2022 10:01 PM

--- exercises.js
+++ exercises.js
@@ -27,3 +27,10 @@ function repeat(n, action) {
   }
   
   repeat(3, console.log);
+
+
+  // Use the reduce method in combination with the concat method to “flatten” an array of arrays into a single array that has all the elements of the original arrays.
+  let arrays = [[1,2,3], [4,5], [6]]
+function flattening(inputList) {
+
+}
