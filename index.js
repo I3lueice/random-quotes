8a89b87f0b375076e23ae7ece31d9033227c2ca8
I3lueice <97864265+I3lueice@users.noreply.github.com>
2022-02-07 20:46:15 -0500

    February 7, 2022 8:46 PM

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let quotes = [`"This is my favorite quote."`,
 `"This is my second favorite quote."`,
 `"This is my third favorite quote."`]
-console.log(Math.random())
-console.log((3 * Math.random()))
-console.log(Math.floor((3 * Math.random())))
-let index = Math.floor((3 * Math.random()))
-console.log(quotes[1])
+// console.log(Math.random())
+// console.log((3 * Math.random()))
+// console.log(Math.floor((3 * Math.random())))
+let index = Math.floor((quotes.length * Math.random()))
+console.log(quotes[index])
